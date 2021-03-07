986380242253eb25176628283f6f7f96961a172e
Sara Ibrahim <sarahkalmm1902@gmail.com>
2021-03-07 04:10:51 +0000

    -Privatized the Discord bot token -Added random study encouragement function to robot -Added private message function to robot -Added welcoming private message to robot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
-import time
+import os
+import random
+# import time
 
 client = discord.Client()
 
@@ -8,27 +10,30 @@ client = discord.Client()
 #Can add encouraging messages while timer is counting down
 
 
-@client.event
-async def on_ready():
-    print('Hello I am {0.user}!'.format(client))
+# @client.event
+# async def on_ready():
+#     print('Hello I am {0.user}!'.format(client))
 
     
 
-def timer_start_break(t):
-    while t:
-        mins, secs = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
-        time.sleep(1)
-        t -= 1
+# def timer_start_break(t):
+#      while t:
+#         mins, secs = divmod(t, 60)
+#         timer = '{:02d}:{:02d}'.format(mins, secs)
+#         print(timer, end="\r")
+#         time.sleep(1)
+#         t -= 1
 
-    print("Take a break!!!!")
+#      print("Take a break!!!!")
 
 
-# function call
-#TODO: change the input to 1500 seconds
-t = 10
-timer_start_break(t)
+#  function call
+# TODO: change the input to 1500 seconds
+# t = 1
+# timer_start_break(t)
+
+
+study_encouragements = ["You can do it!", "You got this!", "Do your best!", "Shoot for the stars!", "I believe in you!", " It always seems impossible until it’s done.", "Remember to take breaks!", "Believe you can and you’re halfway there.", "Strive for progress, not perfection."]
 
 
 @client.event
@@ -39,5 +44,22 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
+    if message.content.startswith('!inspire'):
+      response = random.choice(study_encouragements)
+      await message.channel.send(response)
 
-client.run("ODE3ODQ4MjY0MDMxMjA3NDc1.YEPejQ.7-Q7kOtk_q66cV9GQM_Z6-1FfEE")
+    if message.content.startswith("pomodoro!"):
+      await message.author.send("Let's go!")
+
+    
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, I am Pomodorobo! I will be your study buddy and remind you to take frequent breaks!'
+        )
+
+client.run(os.getenv('TOKEN'))
+
